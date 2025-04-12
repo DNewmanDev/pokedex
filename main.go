50d808e205cc920ce97fa170ba056286f8182751
DonJNewman <Dnewman1934@yahoo.com>
2025-04-12 16:18:03 -0500

    working repl

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-
+	scanner := bufio.NewScanner(os.Stdin)
+	for i := 0; ; i++ {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned := cleanInput(input)
+		fmt.Printf("Your command was: '%s' \n", cleaned[0])
+	}
 }
 
 func cleanInput(input string) []string {
