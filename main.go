8ecbf378046d0996d4d7a77e0cec56321f2893a0
DonJNewman <Dnewman1934@yahoo.com>
2025-04-13 20:15:11 -0500

    working help exit REPL commands

--- main.go
+++ main.go
@@ -13,9 +13,22 @@ func main() {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := scanner.Text()
+		if len(input) == 0 {
+			continue
+		}
 		cleaned := cleanInput(input)
-		fmt.Printf("Your command was: %s \n", cleaned[0])
+
+		if cmd, exists := pokecommands[cleaned[0]]; exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("unknown command")
+		}
+
 	}
+
 }
 
 func cleanInput(input string) []string {
@@ -25,4 +38,44 @@ func cleanInput(input string) []string {
 	trimmedInput := strings.Fields(strings.ToLower(input))
 
 	return trimmedInput
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var pokecommands map[string]cliCommand
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range pokecommands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+
+	}
+	return nil
+}
+
+func init() {
+
+	pokecommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
