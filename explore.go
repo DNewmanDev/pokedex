a56e85714d768931676576ca6c56610ab981d68e
Don Newman <Dnewmandev@gmail.com>
2025-10-06 13:13:42 -0500

    Update explore.go

--- explore.go
+++ explore.go
@@ -6,14 +6,9 @@ import (
 )
 
 func commandExplore(cfg *Config, parameter string) error {
-	//somehow the user input has to get here for
-	//us to know where to explore
-	//for now while building exp functionality
-	//will use dummy url
+
 
 	url := "https://pokeapi.co/api/v2/location-area/" + parameter
-	//check later to see if I need to add
-	//extra fields to config
 
 	resp, err := cfg.Client.GetRequest(url)
 	if err != nil {
@@ -26,7 +21,7 @@ func commandExplore(cfg *Config, parameter string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("Here's ya pokemon")
+	fmt.Println("You found:")
 	for _, result := range exploreresp.PokemonEncounters {
 		fmt.Println("-" + result.Pokemon.Name)
 	}
